d596eb08e162d45805a792ea9f44f36fd8a9f718
Aleksey Voko <voko.aleksey@gmail.com>
2020-04-16 01:31:23 +0400

    Add Spinner and ArrayAdapter

--- app/src/main/java/mr/booroondook/spinner_and_arrayadapter/MainActivity.java
+++ app/src/main/java/mr/booroondook/spinner_and_arrayadapter/MainActivity.java
@@ -3,12 +3,41 @@ package mr.booroondook.spinner_and_arrayadapter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements AdapterView.OnItemSelectedListener {
+    private TextView textView;
+    private String[] lastNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lastNames = getResources().getStringArray(R.array.last_names);
+        textView = findViewById(R.id.text_view);
+        Spinner spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, lastNames);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        textView.setText(lastNames[position]);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        textView.setText("");
     }
 }
